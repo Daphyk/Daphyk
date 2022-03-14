d686178d3c3331b3242fe36be2897535094e66b5
kendid <81610710+Daphyk@users.noreply.github.com>
2022-03-14 23:53:35 +0300

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,25 @@
-
+(function($) {
+    "use strict";
+     $(document).on('ready', function() {	
+	
+			
+		/*====================================
+			Header Sticky JS
+		======================================*/ 
+		jQuery(window).on('scroll', function() {
+			if ($(this).scrollTop() > 1) {
+				$('.header').addClass("sticky");
+			} else {
+				$('.header').removeClass("sticky");
+			}
+		});
+		
+		/*====================================
+			Mobile Menu JS
+		======================================*/ 
+		$('.main-menu').slicknav({
+			prependTo:".mobile-nav",
+			label: '',
+			duration: 500,
+			easingOpen: "easeOutBounce",
+		});
